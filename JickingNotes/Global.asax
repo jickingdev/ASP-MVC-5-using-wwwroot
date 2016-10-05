20f9436de3a8b7b484f5c3e0e54724e5361c8dba
jicking <jdbebiro@gmail.com>
2016-10-05 23:22:50 +0800

    init Signed-off-by: jicking <jdbebiro@gmail.com>

---
+++ JickingNotes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JickingNotes.MvcApplication" Language="C#" %>
---
+++ JickingNotes/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using System.IO;
+
+[assembly: OwinStartup(typeof(JickingNotes.Startup))]
+
+namespace JickingNotes
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            var physicalFileSystem = new PhysicalFileSystem(Path.Combine(root, "wwwroot"));
+            var options = new FileServerOptions
+            {
+                RequestPath = PathString.Empty,
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = false;
+            app.UseFileServer(options);
+        }
+    }
+}
